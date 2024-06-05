1140bca1a83da24c3c40a3b476aeec7b20367599
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-06-06 02:53:13 +0700

    refactor: wrap user script in compiler function

--- test/spec/isolate/built-ints/script-runner.spec.js
+++ test/spec/isolate/built-ints/script-runner.spec.js
@@ -1,24 +1,24 @@
 
 const { expect } = require("chai");
 const { createNewIsolatedContext } = require("../../../../lib/isolate/isolate.js")
-const { runScript } = require("../../../../lib/isolate/script-runner.js");
+const { runSafeScript } = require("../../../../lib/isolate/script-runner.js");
 
 describe('ScriptRunner', () => {
     let isolate, context
     beforeEach(async () => {
         ({ isolate, context } = await createNewIsolatedContext())
     })
-    describe('.runScript', () => {
+    describe('.runSafeScript', () => {
         it('throws if script has a syntax error', (done) => {
             const script = 'function name() {'
-            runScript({ isolate, script }).catch((e) => {
+            runSafeScript({ isolate, script }).catch((e) => {
                 expect(e.message.startsWith('Unexpected end of input')).to.be.true
                 done()
             })
         })
         it('compiles a script', async () => {
             const script = 'function name() {}'
-            await runScript({ isolate, script, context })
+            await runSafeScript({ isolate, script, context })
         })
     })
 })
