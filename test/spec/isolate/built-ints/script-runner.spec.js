4e0224af02be610329e3975b42206587a2900cd7
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-06-08 00:03:32 +0700

    refactor: name script runner methods

--- test/spec/isolate/built-ints/script-runner.spec.js
+++ test/spec/isolate/built-ints/script-runner.spec.js
@@ -1,7 +1,7 @@
 
 const { expect } = require("chai");
 const { createNewIsolatedContext, callFunctionWithArguments, createTransferableReference, copyValueFromReference } = require("../../../../lib/isolate/isolate.js")
-const { runSafeScript, runUnsafeScript } = require("../../../../lib/isolate/script-runner.js");
+const { installSafeScript, installRunnableScript } = require("../../../../lib/isolate/script-runner.js");
 
 async function executeAndGetResult(context) {
     const inputRef = createTransferableReference({ args: [] })
@@ -14,42 +14,42 @@ describe('ScriptRunner', () => {
     beforeEach(async () => {
         ({ isolate, context } = await createNewIsolatedContext())
     })
-    describe('.runSafeScript', () => {
+    describe('.installSafeScript', () => {
         it('throws if script has a syntax error', (done) => {
             const script = 'function name() {'
-            runSafeScript({ isolate, script }).catch((e) => {
+            installSafeScript({ isolate, script }).catch((e) => {
                 expect(e.message.startsWith('Unexpected end of input')).to.be.true
                 done()
             })
         })
         it('adds safe scripts to the global environment', async () => {
             const script1 = 'function name() { return 101 }'
-            await runSafeScript({ isolate, script: script1, context })
+            await installSafeScript({ isolate, script: script1, context })
             const script2 = 'return global.name()'
-            await runUnsafeScript({ isolate, script: script2, context })
+            await installRunnableScript({ isolate, script: script2, context })
             const result = await executeAndGetResult(context)
             expect(result).to.equal(101)
         })
     })
-    describe('.runUnsafeScript', () => {
+    describe('.installRunnableScript', () => {
         it('throws if script has a syntax error', (done) => {
             const script = 'function name() {'
-            runUnsafeScript({ isolate, script }).catch((e) => {
+            installRunnableScript({ isolate, script }).catch((e) => {
                 expect(e.message.startsWith('Unexpected end of input')).to.be.true
                 done()
             })
         })
         it('makes the given script executable', async () => {
             const script = 'return 1+1'
-            await runUnsafeScript({ isolate, script, context })
+            await installRunnableScript({ isolate, script, context })
             const result = await executeAndGetResult(context)
             expect(result).to.equal(2)
         })
         it('overrides previous unsafe scripts', async () => {
             let script = 'return 11'
-            await runUnsafeScript({ isolate, script, context })
+            await installRunnableScript({ isolate, script, context })
             script = 'return 1+1'
-            await runUnsafeScript({ isolate, script, context })
+            await installRunnableScript({ isolate, script, context })
             const result = await executeAndGetResult(context)
             expect(result).to.equal(2)
         })
